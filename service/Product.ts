09c7fec2acc3334c23dad5241e10e0d4809f92ee
Juice <3221ju@gmail.com>
2021-07-23 20:25:11 +0900

    up: Product validation 로직 추가

--- service/Product.ts
+++ service/Product.ts
@@ -7,6 +7,22 @@ export class Product {
     this.id = id;
     this.name = name;
     this._price = price;
+
+    this.validation();
+  }
+
+  validation() {
+    if (this.validationId()) {
+      throw new Error('id 로직에 문제 있음');
+    }
+    return null;
+  }
+
+  validationId() {
+    if (typeof this.id !== 'number' && this.id > 0 && !Number.isInteger(this.id)) {
+      return false;
+    }
+    return true;
   }
 
   getRegPrice(): number {
