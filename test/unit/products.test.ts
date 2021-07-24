99fdd2d2b56354187d65ec614895fe7c01fa3c1a
Juice <3221ju@gmail.com>
2021-07-25 03:36:11 +0900

    beforeEach로 테스트 케이스 마다 초기화 된 데이터가 반영되게 설정

--- test/unit/products.test.ts
+++ test/unit/products.test.ts
@@ -2,21 +2,25 @@ import { createProduct } from '../../controllers/product';
 import Product from '../../models/Product';
 import newProducts from '../data/new-product.json'
 import httpMocks from 'node-mocks-http';
+import { Request, Response } from 'express';
 
 Product.create = jest.fn();
+let req: Request, res: Response, next: null;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  req.body = newProducts;
+  next = null;
+});
 
 describe("Product Controller Create", () => {
   it("should have a createProduct function", () => {
     expect(typeof createProduct).toBe("function");
   });
+
   it("should call ProductModel.create", async () => {
-    let req = httpMocks.createRequest();
-    let res = httpMocks.createResponse();
-    let next = null;
-    req.body = newProducts;
     await createProduct(req, res, next);
     const resResult = res._getJSONData();
-    expect(resResult)
-      .toStrictEqual(newProducts);
+    expect(resResult).toStrictEqual(newProducts);
   });
 });
