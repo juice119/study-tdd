922145ac73feb4d8c3387adb5c48a4474396bd33
Juice <3221ju@gmail.com>
2021-07-25 03:30:57 +0900

    httpMocks 추가

--- controllers/product.ts
+++ controllers/product.ts
@@ -1,5 +1,17 @@
+// @ts-ignore
+import * as express from 'express';
 import Product from '../models/Product';
 
-export function createProduct() {
-  Product.create();
+type Request = express.Request;
+type Response = express.Response;
+type Next = express.NextFunction;
+type retType = Request | Next;
+
+export async function createProduct(req: Request, res: Response, next: Next): Promise<retType> {
+  try {
+    const productResult = await Product.create(req.body);
+    return res.json(req.body);
+  } catch (e) {
+    return next(e);
+  }
 }
--- test/unit/products.test.ts
+++ test/unit/products.test.ts
@@ -1,5 +1,7 @@
 import { createProduct } from '../../controllers/product';
 import Product from '../../models/Product';
+import newProducts from '../data/new-product.json'
+import httpMocks from 'node-mocks-http';
 
 Product.create = jest.fn();
 
@@ -7,8 +9,14 @@ describe("Product Controller Create", () => {
   it("should have a createProduct function", () => {
     expect(typeof createProduct).toBe("function");
   });
-  it("should call ProductModel.create", () => {
-    createProduct();
-    expect(Product.create).toBeCalled();
+  it("should call ProductModel.create", async () => {
+    let req = httpMocks.createRequest();
+    let res = httpMocks.createResponse();
+    let next = null;
+    req.body = newProducts;
+    await createProduct(req, res, next);
+    const resResult = res._getJSONData();
+    expect(resResult)
+      .toStrictEqual(newProducts);
   });
 });
